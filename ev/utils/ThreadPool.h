7cd6b79cc6cf69ff8aa5ff2a3f98b7f07cb9416b
JustDoIt0910 <1396812324@qq.com>
2023-03-15 10:25:04 +0800

    update

--- ev/utils/ThreadPool.h
+++ ev/utils/ThreadPool.h
@@ -19,8 +19,9 @@ namespace ev
     public:
         typedef std::function<void()> Task;
 
-        ThreadPool(int poolSize);
+        ThreadPool(int threadNum, int maxQueueSize);
         void start();
+        void stop();
         void run(Task task);
 
     private:
@@ -31,7 +32,8 @@ namespace ev
         std::condition_variable notEmpty_;
         std::condition_variable notFull_;
         std::atomic_bool running_;
-        int poolSize_;
+        int threadNum_;
+        size_t maxQueueSize_;
     };
 }
 
